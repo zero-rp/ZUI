78f3c38f5d2a1192b5488d0df3a109408367461a
ccchz <65992239@qqcom>
2021-05-15 14:46:42 +0800

    删除文件。

--- dukluv/duv_req.h
+++
@@ -1,8 +0,0 @@
-#ifndef DUV_REQ_H
-#define DUV_REQ_H
-
-#include "duv.h"
-
-duk_ret_t duv_cancel(duk_context *ctx);
-
-#endif
--- vc6rt/vc6include/wnet/intrin.h
+++
@@ -1,9 +0,0 @@
-
-#include "windows.h"
-#include "emmintrin.h"
-#include "tmmintrin.h"
-#include "xmmintrin.h"
-#include "pmmintrin.h"
-#include "intrinvc6.h"
-#include "xatomic0.h"
-#include "xatomic.h"
